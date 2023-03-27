f15ef80ee1969015962a09638ad5f8718986fd4f
fpv13 <dzhikser@gmail.com>
2023-03-27 14:18:51 +0300

    update 4

---
+++ MethodReturnOperator.java
@@ -0,0 +1,21 @@
+public class MethodReturnOperator {
+        public static void main(String[] args) {
+    long a = 3;
+    long b = cube(a);
+    System.out.println(b);
+    long c = ninthDegree(3);
+            System.out.println(c);
+    }
+
+        public static long cube(long a){
+        return a*a*a;
+    }
+
+        //напишите тут ваш код
+        public static long ninthDegree(long b){
+            long res =cube(b)*cube(b);
+        return res;
+    }
+    }
+
+
